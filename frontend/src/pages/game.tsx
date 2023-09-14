20fa0b4ec4c9fb75aea941f06853d9948aae968d
Trevor Trinh <50549133+trevor-trinh@users.noreply.github.com>
2023-09-15 02:31:50 +0800

    start event listening

--- frontend/src/pages/game.tsx
+++ frontend/src/pages/game.tsx
@@ -8,12 +8,14 @@ import {
   CardFooter,
   Wrap,
   WrapItem,
+  Button,
 } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-import { Player } from '@/types'
+import { MatchEvent, Player } from '@/types'
 import SignalButton from '@/components/SignalButton'
-import { useAccount } from 'wagmi'
+import { useAccount, useContractEvent } from 'wagmi'
+import ConsensualMessagingJson from '@/lib/abi/ConsensualMessaging.json'
 
 export default function Game(props: any) {
   const router = useRouter()
@@ -21,68 +23,70 @@ export default function Game(props: any) {
 
   const [timeLeft, setTimeLeft] = useState(30)
   const [selectedPlayer, setSelectedPlayer] = useState<Player>()
+  const [showResults, setShowResults] = useState(false)
+  const [matches, setMatches] = useState<MatchEvent[]>([])
+
   const { address, isConnecting, isDisconnected } = useAccount()
 
   // timer for round to end
   // useEffect(() => {
   //   if (timeLeft <= 0) {
-  //     router.push('/results')
+  //     setShowResults(true)
   //   }
   //   const timeoutId = setTimeout(() => setTimeLeft(timeLeft - 1), 1000)
   //   return () => clearTimeout(timeoutId)
   // }, [timeLeft])
 
-  // gets the list of names+addresses from backend
-  const { socket } = props
-
-  // useEffect(() => {
-  //   // fetch('/api/game')
-  //   //   .then((res) => res.json())
-  //   //   .then((data) => {
-  //   //     setPlayers(data)
-  //   //   })
-  //   setPlayers([
-  //     { name: 'player1', address: '0x123' },
-  //     {
-  //       name: 'player2',
-  //       address: '0x456',
-  //     },
-  //   ])
-  // }, [])
-
-  // could listen to events starting here
-  // then when something happens just pass it to the next screen
+  useContractEvent({
+    address: '0xeCB2Ed307793c124706993472514D8E2627D4173',
+    abi: ConsensualMessagingJson.abi,
+    eventName: 'MatchEvent',
+    listener(log) {
+      console.log(log)
+      setMatches((prev) => prev.push(log))
+    },
+  })
 
   return (
     <Layout>
-      <Text fontSize={'3xl'}>Send a signal!</Text>
-      <Text>{timeLeft}s</Text>
-      <Wrap spacing={6}>
-        {people &&
-          people
-            .filter((player: any) => player.address !== address)
-            .map((player: any) => (
-              <WrapItem key={player.name}>
-                <Card textAlign={'center'} w={'3xs'} alignItems={'center'}>
-                  <CardHeader pb={0}>
-                    <Heading size="md" noOfLines={1} maxW={'3xs'}>
-                      {player.name}
-                    </Heading>
-                  </CardHeader>
-                  <CardBody pb={0} noOfLines={1} maxW={'3xs'}>
-                    <Text>{player.address}</Text>
-                  </CardBody>
-                  <CardFooter>
-                    <SignalButton
-                      player={player}
-                      selectedPlayer={selectedPlayer}
-                      setSelectedPlayer={setSelectedPlayer}
-                    />
-                  </CardFooter>
-                </Card>
-              </WrapItem>
-            ))}
-      </Wrap>
+      {!showResults ? (
+        <>
+          <Text fontSize={'3xl'}>Send a signal!</Text>
+          <Text>{timeLeft}s</Text>
+          <Wrap spacing={6}>
+            {people &&
+              people
+                .filter((player: any) => player.address !== address)
+                .map((player: any) => (
+                  <WrapItem key={player.name}>
+                    <Card textAlign={'center'} w={'3xs'} alignItems={'center'}>
+                      <CardHeader pb={0}>
+                        <Heading size="md" noOfLines={1} maxW={'3xs'}>
+                          {player.name}
+                        </Heading>
+                      </CardHeader>
+                      <CardBody pb={0} noOfLines={1} maxW={'3xs'}>
+                        <Text>{player.address}</Text>
+                      </CardBody>
+                      <CardFooter>
+                        <SignalButton
+                          player={player}
+                          selectedPlayer={selectedPlayer}
+                          setSelectedPlayer={setSelectedPlayer}
+                        />
+                      </CardFooter>
+                    </Card>
+                  </WrapItem>
+                ))}
+          </Wrap>
+        </>
+      ) : (
+        <>
+          <Text fontSize={'3xl'}>It's a Match!</Text>
+
+          <Button onClick={() => router.push('/')}>Play Again</Button>
+        </>
+      )}
     </Layout>
   )
 }
--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -2,3 +2,9 @@ export type Player = {
   name: string
   address: string
 }
+
+export type MatchEvent = {
+  from: string
+  to: string
+  message: string
+}
