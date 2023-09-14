cf72b07a465e065f4813d6232d0430780fe857a2
Timothy G <timg51237@gmail.com>
2023-09-14 18:48:01 +0800

    types

--- frontend/src/pages/lobby.tsx
+++ frontend/src/pages/lobby.tsx
@@ -1,4 +1,5 @@
 import Layout from '@/components/Layout'
+import { Player } from '@/types'
 import { Text, Button, UnorderedList, ListItem } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
@@ -7,7 +8,7 @@ export default function Lobby(props: any) {
   const router = useRouter()
   const { socket } = props
   const { name, room } = router.query
-  const [people, setPeople] = useState([])
+  const [people, setPeople] = useState<Player[]>([])
 
   useEffect(() => {
     if (!router.query.name) {
@@ -29,7 +30,7 @@ export default function Lobby(props: any) {
       <Text>Hello {name}!</Text>
       <Text fontSize={'4xl'}>{room}</Text>
       <UnorderedList>
-        {people.map((person: any) => (
+        {people.map((person: Player) => (
           <ListItem>{person.name}</ListItem>
         ))}
       </UnorderedList>
